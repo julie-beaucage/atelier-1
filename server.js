b419e5fbed751fde052b15300c3dea11435a469b
julie-beaucage <201386196@edu.clg.qc.ca>
2024-08-27 15:20:08 -0400

    changement sfinaux

--- server.js
+++ server.js
@@ -38,11 +38,6 @@ const server = http.createServer((req, res) => {
     }
   }
 
-  /*
-console.log(req.url);
-let reqInfo = {url:req.url, method:req.method};
- res.writeHead(200, {"Content-Type": "application/json"});
- res.end(JSON.stringify(reqInfo));*/
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
